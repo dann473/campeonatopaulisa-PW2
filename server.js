9f6c921b7619aea086ae1fabac5af3ce9e0746b6
yoBiro <bielzingg92@gmail.com>
2026-05-27 07:46:50 -0300

    fix: url in the CORS

--- server.js
+++ server.js
@@ -10,7 +10,7 @@ const app = express()
 const router = express.Router()
 
 app.use(cors({
-    origin: ['http://localhost:5500']
+    origin: ['http://127.0.0.1:5500']
 }));
 
 app.get('/', (req, res) => {
